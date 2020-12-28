dfdb3866ba61ff780197b6b62c87a272e39c1c05
haoweili1096 <haoweili007@gmail.com>
2020-12-27 19:38:23 -0800

    Add price, BuildControls and BuildControl. closes #6.

---
+++ src/components/Sandwich/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Meat', type: 'meat'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Lettuce', type: 'lettuce'},
+    {label: 'Ham', type: 'ham'},
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}/>
+        ))}
+    </div>
+);
+
+export default buildControls;
--- src/containers/SandwichBuilder/SandwichBuilder.js
+++ src/containers/SandwichBuilder/SandwichBuilder.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Sandwich from '../../components/Sandwich/Sandwich';
+import BuildControls from '../../components/Sandwich/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    lettuce: 0.5,
+    ham: 0.7,
+    cheese: 0.4,
+    meat: 1.3
+}
 
 class SandwichBuilder extends Component {
     state = {
@@ -9,14 +17,71 @@ class SandwichBuilder extends Component {
             ham: 1,
             cheese: 2,
             meat: 2
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
         }
+        updatedIngredients[type] = updatedCount;
+
+        const oldPrice = this.state.totalPrice;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState(
+            {
+                ingredients: updatedIngredients,
+                totalPrice: newPrice
+            }
+        );
+    }
+
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        //can not be smaller than 0
+        if(oldCount <= 0){
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount;
+
+        const oldPrice = this.state.totalPrice;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState(
+            {
+                ingredients: updatedIngredients,
+                totalPrice: newPrice
+            }
+        )
     }
 
     render () {
+        const disabledInfo = {
+            ...this.props.ings
+        };
+        for(let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+         
         return (
             <Aux>
                 <Sandwich ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls
+                    price={this.state.totalPrice}
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </Aux>
         );
     }
